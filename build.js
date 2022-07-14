b75dad5071e094bdae9b18b7103994dea705fe58
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2022-07-14 23:24:55 +0200

    Build. Use helper.js funxtions. Remove fse, picocolors.

--- build.js
+++ build.js
@@ -7,8 +7,8 @@ const pathBuildKram = path.resolve("../buildKramGhsvs");
 
 const helper = require(`${pathBuildKram}/build/helper.js`);
 
-const pc = require(`${pathBuildKram}/node_modules/picocolors`);
-const fse = require(`${pathBuildKram}/node_modules/fs-extra`);
+// const pc = require(`${pathBuildKram}/node_modules/picocolors`);
+// const fse = require(`${pathBuildKram}/node_modules/fs-extra`);
 
 const source = `./node_modules`;
 const target = `./dist`;
@@ -29,119 +29,62 @@ let targetDir = '';
 	await helper.cleanOut(cleanOuts);
 
 	to = target
-	await fse.mkdir(to).then(
-		answer => console.log(pc.yellow(pc.bold(`Created "${to}".`)))
-	);
+	await helper.mkdir(to);
 
 	// Subfolders to dist.
 	from = './custom';
 	to = path.join(target);
-	await fse.copy(from, to
-	).then(
-		answer => console.log(
-			pc.yellow(pc.bold(`Copied "${from}" to "${to}".`))
-		)
-	);
+	await helper.copy(from, to)
 
 	// JAVASCRIPT STARTS HERE.
 	targetDir = 'js';
 
 	from = './custom/js';
 	to = path.join(target, targetDir);
-	await fse.copy(from, to
-	).then(
-		answer => console.log(
-			pc.yellow(pc.bold(`Copied "${from}" to "${to}".`))
-		)
-	);
+	await helper.copy(from, to)
 
 	from = path.join(source, 'venobox/dist/venobox.min.js');
 	to = path.join(target, targetDir, 'venobox/venobox.min.js');
-	await fse.copy(from, to
-	).then(
-		answer => console.log(
-			pc.yellow(pc.bold(`Copied "${from}" to "${to}".`))
-		)
-	);
+	await helper.copy(from, to)
 
 	from = path.join(source, 'bootstrap/dist/js');
 	to = path.join(target, targetDir, 'bootstrap');
-	await fse.copy(from, to
-	).then(
-		answer => console.log(
-			pc.yellow(pc.bold(`Copied "${from}" to "${to}".`))
-		)
-	);
+	await helper.copy(from, to)
 
 	from = path.join(source, 'anchor-js/anchor.min.js');
 	to = path.join(target, targetDir, 'anchor-js/anchor.min.js');
-	await fse.copy(from, to
-	).then(
-		answer => console.log(
-			pc.yellow(pc.bold(`Copied "${from}" to "${to}".`))
-		)
-	);
+	await helper.copy(from, to)
 
 	from = path.join(source, 'clipboard/dist/clipboard.min.js');
 	to = path.join(target, targetDir, 'clipboard/clipboard.min.js');
-	await fse.copy(from, to
-	).then(
-		answer => console.log(
-			pc.yellow(pc.bold(`Copied "${from}" to "${to}".`))
-		)
-	);
+	await helper.copy(from, to)
 
 	// CSS STARTS HERE.
 	targetDir = 'css';
 
 	from = path.join(source, 'venobox/dist/venobox.min.css');
 	to = path.join(target, targetDir, 'venobox/venobox.min.css');
-	await fse.copy(from, to
-	).then(
-		answer => console.log(
-			pc.yellow(pc.bold(`Copied "${from}" to "${to}".`))
-		)
-	);
+	await helper.copy(from, to)
 
 	// SCSS STARTS HERE.
 	targetDir = 'scss';
 
 	from = './custom/scss';
 	to = path.join(target, targetDir);
-	await fse.copy(from, to
-	).then(
-		answer => console.log(
-			pc.yellow(pc.bold(`Copied "${from}" to "${to}".`))
-		)
-	);
+	await helper.copy(from, to)
 
 	from = path.join(source, 'bootstrap/scss');
 	to = path.join(target, targetDir, 'bootstrap');
-	await fse.copy(from, to
-	).then(
-		answer => console.log(
-			pc.yellow(pc.bold(`Copied "${from}" to "${to}".`))
-		)
-	);
+	await helper.copy(from, to)
 
 	// SVGS STARTS HERE.
 	targetDir = 'svgs';
 
 	from = `${svgs}/bi`;
 	to = path.join(target, targetDir, 'bi');
-	await fse.copy(from, to
-	).then(
-		answer => console.log(
-			pc.yellow(pc.bold(`Copied "${from}" to "${to}".`))
-		)
-	);
+	await helper.copy(from, to)
 
 	from = `${svgs}/icons-overview.html`;
 	to = path.join(target, targetDir, 'icons-overview.html');
-	await fse.copy(from, to
-	).then(
-		answer => console.log(
-			pc.yellow(pc.bold(`Copied "${from}" to "${to}".`))
-		)
-	);
+	await helper.copy(from, to)
 })();
